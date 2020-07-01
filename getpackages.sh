ce6f0aa8a1d3b1dbc63a6ce92511f6dba61a2b4d
Gilberto Vidal <gilbertovidala@gmail.com>
2020-07-01 12:05:25 -0500

    Updated getpackages.sh

--- getpackages.sh
+++ getpackages.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 PACKAGES="\
-	gcc make\
+	adduser sudo\
+	gcc make man\
 	bash-completion git\
 	alsa-utils pulseaudio\
-	thunar dbus-x11 dunst libnotify-bin\
-	feh xzoom cmus curl gpick screenfetch fim slop maim zathura sxiv p7zip-full\
-	xsel xclip\
-	ngnix\
+	dbus-x11 dunst fonts-noto-color-emoji geany libnotify-bin thunar\
+	bc cmus curl feh ffmpeg fim freerdp2-x11 gpick maim mednafen nnn ntfs-3g openfortivpn p7zip-full python3 rofi screenfetch slop sxiv tigervnc-viewer tmux xzoom zathura\
+	xclip xsel xdotool xinit xorg\
 	nethogs nload\
-	libx11-dev libxinerama-dev libxft-dev libjpeg-dev libxrandr-dev libxt-dev\
+	libncurses5-dev libx11-dev libxinerama-dev libxft-dev libjpeg-dev libxrandr-dev libxt-dev\
+	firmware-realtek\
 	firmware-amd-graphics firmware-linux-nonfree libgl1-mesa-dri libglx-mesa0 mesa-vulkan-drivers xserver-xorg-video-all \
 	"
 
 for package in $PACKAGES; do
 	echo $package
 done
+sudo apt install $PACKAGES
