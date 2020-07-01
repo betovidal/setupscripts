89de681d36e11574052c7a48d8d54110d29dc225
Gilberto Vidal <gilbertovidala@gmail.com>
2020-07-01 18:14:49 -0500

    Added creation folder script.

---
+++ createfolders.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p $HOME/.vim/html
+mkdir -p $HOME/.vim/tags
+mkdir -p $HOME/.vim/tmp
+mkdir -p $HOME/.vim/undo
+
+mkdir -p $HOME/Development
+mkdir -p $HOME/Download
+mkdir -p $HOME/Pictures/caps
+mkdir -p $HOME/Development
+
+sudo mkdir /opt/vim8
