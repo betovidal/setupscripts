a3dddc33ecc368e113cb694f5bb3fb43e0711ab2
Gilberto Vidal <gilbertovidala@gmail.com>
2020-07-01 23:58:15 -0500

    Fixed folder creation.

--- createfolders.sh
+++ createfolders.sh
@@ -6,8 +6,8 @@ mkdir -p $HOME/.vim/tmp
 mkdir -p $HOME/.vim/undo
 
 mkdir -p $HOME/Development
-mkdir -p $HOME/Download
+mkdir -p $HOME/Downloads
 mkdir -p $HOME/Pictures/caps
 mkdir -p $HOME/Remotes
 
-sudo mkdir /opt/vim8
+sudo mkdir -p /opt/vim8
