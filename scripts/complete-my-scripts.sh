82e78d625c229a7ba5ae4c16110426c1da40cf0b
Gilberto Vidal <gilbertovidala@gmail.com>
2020-07-03 16:44:41 -0500

    Fixed vars in ifs for complete script. Print extension along conf files

--- scripts/complete-my-scripts.sh
+++ scripts/complete-my-scripts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+RDC="rdc.sh"
+WIFICONNECT="wificonnect.sh"
 _file_completions() {
 	# If there's already an argument after the command, do nothing
 	local variant=$1
@@ -7,19 +8,15 @@ _file_completions() {
 	if [ "${#COMP_WORDS[@]}" != "2" ]; then
 		return
 	fi
-	if [ $variant == 'rdc' ]; then
+	if [ "$variant" == "$RDC" ]; then
 		source_dir=~/Remotes/*
-	elif [ $variant == 'wificonnect' ]; then
+	elif [ "$variant" == "$WIFICONNECT" ]; then
 		source_dir=~/Networks/*.conf
 	fi
 	filenames=""
 	is_first=1
 	for fileroute in $source_dir; do
-		if [ $variant == 'rdc' ]; then
-			filename=$(echo $fileroute | awk -F / '{ print $NF }')
-		elif [ $variant == 'wificonnect' ]; then
-			filename=$(echo $fileroute | awk -F - '{ print $NF }' | awk -F . '{ print $1}')
-		fi
+		filename=$(echo $fileroute | awk -F / '{ print $NF }')
 		if [ $is_first != 1 ]; then
 			filenames+=' '
 		fi
@@ -28,5 +25,5 @@ _file_completions() {
 	done
 	COMPREPLY=($(compgen -W "$filenames" "${COMP_WORDS[1]}"))
 }
-complete -F _file_completions "rdc" rdc
-complete -F _file_completions "wificonnect" wificonnect
+complete -F _file_completions "$RDC" "$RDC"
+complete -F _file_completions "$WIFICONNECT" "$WIFICONNECT"
