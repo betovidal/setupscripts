db49a7b70f8d8872578eab6b4b37faac45a73067
Gilberto Vidal <gilbertovidala@gmail.com>
2020-07-28 15:35:35 -0500

    Removed file decorations wificonnect and print assumption message

--- scripts/wificonnect.sh
+++ scripts/wificonnect.sh
@@ -3,7 +3,7 @@ if [ -z "$1" ]; then
 	echo "Provide a network configuration file name."
 	exit -1
 fi
-ROUTE=~/Networks/wlan0-$1.conf
+ROUTE=~/Networks/$1
 if [ ! -f $ROUTE ]; then
 	echo "Configuration file $ROUTE not found!"
 	exit -2
@@ -15,6 +15,7 @@ if sudo -lS &> /dev/null << EOF
 $PASSWD
 EOF
 then
+	echo "Assuming interface wlan0"
 	sudo pkill wpa_supplicant
 	sudo pkill dhclient
 	sudo wpa_supplicant -B -i wlan0 -c $ROUTE
