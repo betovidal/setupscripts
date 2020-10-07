a27618c686ec88f78bab6719bec023f086c0e160
Gilberto Vidal <gilbertovidala@gmail.com>
2020-10-07 10:31:45 -0500

    Format packages and added bluetooth support

--- getpackages.sh
+++ getpackages.sh
@@ -3,14 +3,20 @@ PACKAGES="\
 	adduser sudo\
 	gcc make man\
 	bash-completion git\
-	alsa-utils pulseaudio\
-	dbus-x11 dunst fonts-noto-color-emoji fonts-symbola geany libnotify-bin thunar ffmpegthumbnailer tumbler-plugins-extra\
-	bc cmus curl feh ffmpeg fim freerdp2-x11 gpick maim mednafen nnn ntfs-3g openfortivpn p7zip-full python3 rofi screenfetch slop sxiv tigervnc-viewer tmux xzoom zathura\
+	alsa-utils pulseaudio pulseaudio-module-bluetooth\
+	fonts-noto-color-emoji fonts-symbola\
+	dbus-x11 dunst libnotify-bin ffmpegthumbnailer tumbler-plugins-extra\
+	geany thunar mednafen zathura xzoom gpick\
+	bc cmus curl feh ffmpeg fim freerdp2-x11 maim nnn ntfs-3g\
+	openfortivpn p7zip-full python3 rofi screenfetch slop sxiv\
+	tigervnc-viewer tmux\
 	xclip xsel xdotool xinit xorg\
 	nethogs nload\
-	libncurses5-dev libx11-dev libxinerama-dev libxft-dev libjpeg-dev libxrandr-dev libxt-dev\
+	libncurses5-dev libx11-dev libxinerama-dev libxft-dev\
+	libjpeg-dev libxrandr-dev libxt-dev\
 	firmware-realtek\
-	firmware-amd-graphics firmware-linux-nonfree libgl1-mesa-dri libglx-mesa0 mesa-vulkan-drivers xserver-xorg-video-all \
+	firmware-amd-graphics firmware-linux-nonfree libgl1-mesa-dri\
+	libglx-mesa0 mesa-vulkan-drivers xserver-xorg-video-all\
 	"
 
 for package in $PACKAGES; do
