4b3fd421861d57c114b160e68d3112c658cec66b
Gilberto Vidal <gilbertovidala@gmail.com>
2021-05-17 12:55:44 -0500

    Added script to show current cmus track info in a dunst pop up.

---
+++ dotfiles/.local/bin/track-info.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+INFO=$(cmus-remote -Q)
+NOTRUNNING="cmus-remote: cmus is not running"
+STOPPED="status stopped"
+NOTFOUND="command not found"
+TIME=1500
+get_tag () {
+       INFO="$1"
+       TAG="$2"
+       LINE=$(echo "$INFO" | grep "$TAG")
+       VALUE=$(echo $LINE | sed -n "s/^$TAG//p")
+       echo $VALUE
+}
+show_message() {
+       notify-send -t "$TIME" "$1" "$2"
+}
+if [[ "$INFO" =~ "$NOTRUNNING" ]]; then
+       show_message "Not found"
+elif [[ "$INFO" =~ "$STOPPED" ]]; then
+       show_message "No track is playing"
+elif [[ "$INFO" =~ "$NOTFOUND" ]]; then
+       show_message "cmus is not installed"
+else
+       ARTISTTAG="tag artist "
+       TITLETAG="tag title "
+       ARTIST=$(get_tag "$INFO" "$ARTISTTAG")
+       TITLE=$(get_tag "$INFO" "$TITLETAG")
+       show_message "$ARTIST" "$TITLE"
+fi
