8cd37e5558aafe937ea64a3b15095830019cd727
Gilberto Vidal <gilbertovidala@gmail.com>
2021-08-10 18:53:48 -0500

    Added AUR repos. Ignore some suckless repos. Echo message on vim-plug.

--- getrepos.sh
+++ getrepos.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 REPOS_FOLDER=$HOME/Repos/
 DEV_FOLDER=$HOME/Development/
+AUR_FOLDER=$HOME/AUR/
 if [ ! -d $REPOS_FOLDER ]; then
 	mkdir -p $REPOS_FOLDER
 fi
 if [ ! -d $DEV_FOLDER ]; then
 	mkdir -p $DEV_FOLDER
 fi
+if [ ! -d $AUR_FOLDER ]; then
+	mkdir -p $AUR_FOLDER
+fi
 # Suckless tools
-SUCKLESSTOOLS="dmenu dwm farbfeld sent slock slstatus st"
+_SUCKLESSTOOLS="dwm farbfeld sent slstatus"
+SUCKLESSTOOLS="dmenu slock st"
 echo "= = = = = Suckless Tools = = = = ="
 for tool in $SUCKLESSTOOLS; do
 	git clone https://git.suckless.org/$tool $REPOS_FOLDER$tool
@@ -17,6 +22,13 @@ done
 echo "= = = = = Additional Repositories = = = = ="
 git clone https://github.com/vim/vim.git $REPOS_FOLDER"vim"
 git clone https://github.com/cspeterson/splatmoji.git $REPOS_FOLDER"splatmoji"
+# AUR repos
+echo "= = = = = AUR packages = = = = ="
+AURREPOS="libxft-bgra minecraft-launcher nomachine tuxguitar"
+for aurrepo in $AURREPOS; do
+	git clone https://aur.archlinux.org/$aurrepo.git $AUR_FOLDER$aurrepo
+done
+
 # My repos
 echo "= = = = = My Own Repositories = = = = ="
 MYREPOS="setupscripts"
@@ -24,4 +36,6 @@ for myrepo in $MYREPOS; do
 	git clone https://github.com/betovidal/$myrepo.git $DEV_FOLDER$myrepo
 done
 
+# Setup vim-plug
+echo "= = = = = Setting up vim-plug = = = = ="
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
