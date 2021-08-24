4c8a30a22f6eb19833e85299c6c9a2e5ec4e7dbb
Gilberto Vidal <gilbertovidala@gmail.com>
2021-08-24 17:00:37 -0500

    Added --no-folding in stow call (link.sh) to avoid linking folders and link files only.

--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # So, this and all.sh should be executed from the parent folder
 # unlink with the same command, adding -D flag
-stow "dotfiles" -t $HOME
+stow --no-folding "dotfiles" -t $HOME
