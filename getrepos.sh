7ec0da66b72323ab2b22fb2651fb0808715ad018
Gilberto Vidal <gilbertovidala@gmail.com>
2021-10-06 01:43:56 -0500

    Readded vim repo

--- getrepos.sh
+++ getrepos.sh
@@ -20,12 +20,12 @@ for tool in $SUCKLESSTOOLS; do
 done
 # Other repos
 echo "= = = = = Additional Repositories = = = = ="
-# git clone https://github.com/vim/vim.git $REPOS_FOLDER"vim"
+git clone https://github.com/vim/vim.git $REPOS_FOLDER"vim"
 git clone https://github.com/cspeterson/splatmoji.git $REPOS_FOLDER"splatmoji"
 git clone https://github.com/toadjaune/pulseaudio-config.git $REPOS_FOLDER"pulseaudio-config"
 # AUR repos
 echo "= = = = = AUR packages = = = = ="
-AURREPOS="lemonbar-git libxft-bgra minecraft-launcher nomachine tuxguitar xp-pen-tablet title"
+AURREPOS="lemonbar-git libxft-bgra minecraft-launcher nomachine pod2man tuxguitar xp-pen-tablet xtitle"
 for aurrepo in $AURREPOS; do
 	git clone https://aur.archlinux.org/$aurrepo.git $AUR_FOLDER$aurrepo
 done
