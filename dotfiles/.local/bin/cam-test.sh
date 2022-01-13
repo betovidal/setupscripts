4bc3819a70ab3df011a5f6c31ae22604dfd0775f
Gilberto Vidal <gilbertovidala@gmail.com>
2022-01-12 23:42:52 -0600

    cam-test accepts an optional numeric parameter to select a specific device.

--- dotfiles/.local/bin/cam-test.sh
+++ dotfiles/.local/bin/cam-test.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-ffplay -f v4l2 -i /dev/video0
+DEVNUM="0"
+# List only
+if [ "$1" = "-l" ]; then
+	find /dev/ -iname "video*"
+	exit
+fi
+# Default to zero
+if [ ! -z "$1" ]; then
+	DEVNUM="$1"
+fi
+# Use specified dev num
+DEV="/dev/video$DEVNUM"
+# Check if character file device exists
+# https://tldp.org/LDP/abs/html/fto.html
+if [ ! -c  $DEV ]; then
+	echo "$DEV device does not exist!"
+	exit
+fi
+echo "Using device $DEV"
+ffplay -f v4l2 -i "$DEV"
