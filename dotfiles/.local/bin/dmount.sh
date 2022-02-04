8e8094ddb872cc9be0205617c9499f4beaf65ae6
Gilberto Vidal <gilbertovidala@gmail.com>
2022-02-04 00:46:56 -0600

    Removed password check inside (call with sudo). Better checks and better messages.

--- dotfiles/.local/bin/dmount.sh
+++ dotfiles/.local/bin/dmount.sh
@@ -1,65 +1,37 @@
-#!/bin/bash
-# ask_for_permissions() {
-# }
-execute_command() {
-	cmd=$1
-	echo "Executing: \$$cmd"
-	$cmd
-}
+#!/bin/env bash
+# Should be run with sudo
+USERFOLDER="$USER"
+if [ "$USERFOLDER" == "root" ]; then
+	USERFOLDER="$SUDO_USER"
+fi
 do_mount() {
 	for disk in "$@"; do
 		ROUTE="/dev/$disk"
-		if [ -a $ROUTE ]; then
+		if [ -b $ROUTE ]; then
 			LABEL=$(ls -l /dev/disk/by-label | grep $disk | awk '{ print $9 }' )
 			if [ -z $LABEL ]; then
 				echo "Can't mount a disk without a label."
 				continue
 			fi
-			FOLDER="/media/$USER/$LABEL"
-			echo "Mounting : $ROUTE"
-			echo "Label    : $LABEL"
-			echo "User     : $USER"
-			echo "Folder   : $FOLDER"
-			if [ ! -d "$FOLDER" ]; then
-				echo "Need to create directory."
-				sudo mkdir -p "$FOLDER"
-			else
-				echo "Directory exists."
+			MOUNTPOINT="/media/$USERFOLDER/$LABEL"
+			if [ ! -d "$MOUNTPOINT" ]; then
+				mkdir -p "$MOUNTPOINT"
 			fi
-			if [[ $(findmnt -M "$FOLDER") ]]; then
-				echo "Route is in use by another mount!"
+			if [[ $(findmnt -M "$MOUNTPOINT") ]]; then
+				echo "Route is in use by another mount. Can't mount $disk"
 			else
-				sudo mount $ROUTE "$FOLDER"
+				echo "Mounted $disk in $MOUNTPOINT"
+				mount "$ROUTE" "$MOUNTPOINT"
 			fi
 		else
-			echo "Can't find specified disk."
+			echo "Can't find specified disk: $disk"
 		fi
 		echo "- - - - - - - - - -"
 	done
 }
-ask_for_permissions() {
-# !!!!!!! START OF INNER BLOCK (If I indent this, it breaks)
-echo "Enter sudo credentials"
-IFS= read -rs PASSWD
-sudo -k
-if sudo -lS &> /dev/null << EOF
-$PASSWD
-EOF
-then
-	echo "Permission granted"
-	do_mount $@
-else
-	echo 'Wrong password.'
-fi
-# !!!!!!! END OF INNER BLOCK (If I indent this, it breaks)
-}
 if [ -z "$1" ]; then
-	execute_command "lsblk -o +LABEL,FSTYPE"
+	lsblk -o +LABEL,FSTYPE
 	exit 1
-fi
-if sudo -n true; then
-	# echo "You have permissions"
-	do_mount $@
 else
-	ask_for_permissions $@
+	do_mount $@
 fi
