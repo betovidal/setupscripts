d7e5cf2db0b2a012d3d6fbb731c5abe2a25867af
Gilberto Vidal <gilbertovidala@gmail.com>
2022-06-02 12:41:38 -0500

    Longer bash aliases to avoid conflicts with other binaries.

--- dotfiles/.config/aliases.bash
+++ dotfiles/.config/aliases.bash
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
 # git aliases
-alias gp="git pull"
-alias ga="git add"
 alias gall="git add -A"
-alias gst="git status"
-alias gc="git commit -m"
-alias gp="git pull"
-alias go="git push -u origin"
-alias gd="git diff"
+alias gstat="git status"
+alias gcom="git commit -m"
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gget="git remote get-url origin"
+alias gset="git remote set-url origin"
 
 # pacman aliases
 alias pu="sudo pacman -Syu"
+alias psi="sudo pacman -Si"
+alias pqi="pacman -Qi"
 
+# Other tools
 alias ls="ls --color=auto"
 alias bc="bc -l"
-
+alias serve="python -m http.server"
