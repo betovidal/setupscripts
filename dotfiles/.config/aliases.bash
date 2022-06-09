df6c52ef52a0706930bf3d245961e1a5dca539fe
Gilberto Vidal <gilbertovidala@gmail.com>
2022-06-09 18:25:42 -0500

    Added alias for cleaning a git repo.

--- dotfiles/.config/aliases.bash
+++ dotfiles/.config/aliases.bash
@@ -2,6 +2,7 @@
 
 # git
 alias gall="git add -A"
+alias gclear="git clean -df; git reset --hard;"
 alias gstat="git status"
 alias gcom="git commit -m"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
