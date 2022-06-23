2640b839205fa5d842a09663cc0a5a8ab8e40f35
Gilberto Vidal <gilbertovidala@gmail.com>
2022-06-23 11:26:44 -0500

    Fixed linting

--- dotfiles/.local/bin/functions.bash
+++ dotfiles/.local/bin/functions.bash
@@ -31,40 +31,40 @@ n ()
 }
 
 repos () {
-	if [ "$1" == "i" ] || [ "$1" == "install" ]; then
-		for d in */; do
-			echo "Installing $d - - - -"
-			cd "$d" || echo "Could not enter $d"; continue;
-			makepkg -sirc --needed
-			cd ..
-			echo ""
-		done;
-	elif [ "$1" == "u" ] || [ "$1" == "update" ]; then
-		for d in */; do
-			echo "- = Updating $d = -"
-			cd "$d" || echo "Could not enter $d"; continue;
-			git pull
-			cd ..
-			echo ""
-		done;
-	else
-		echo "Available arguments: [i|install]|[u|update]"
-	fi
+    if [ "$1" = "i" ] || [ "$1" = "install" ]; then
+        for d in */; do
+            echo "Installing $d - - - -"
+            cd "$d" || echo "Could not enter $d"; continue;
+            makepkg -sirc --needed
+            cd ..
+            echo ""
+        done;
+    elif [ "$1" = "u" ] || [ "$1" = "update" ]; then
+        for d in */; do
+            echo "- = Updating $d = -"
+            cd "$d" || echo "Could not enter $d"; continue;
+            git pull
+            cd ..
+            echo ""
+        done;
+    else
+        echo "Available arguments: [i|install]|[u|update]"
+    fi
 }
 
 start_web () {
-	sudo systemctl start nginx
-	sudo systemctl start mariadb
-	sudo systemctl start php-fpm
+    sudo systemctl start nginx
+    sudo systemctl start mariadb
+    sudo systemctl start php-fpm
 }
 
 stop_web () {
-	sudo systemctl stop nginx
-	sudo systemctl stop mariadb
-	sudo systemctl stop php-fpm
+    sudo systemctl stop nginx
+    sudo systemctl stop mariadb
+    sudo systemctl stop php-fpm
 }
 
 steam_update_on_launch () {
-	STEAM_FOLDER="$HOME/.steam/steam/steamapps"
-	sed -i 's/\("AutoUpdateBehavior".*\)"0"/\1"1"/' "${STEAM_FOLDER}"/*.acf
+    STEAM_FOLDER="$HOME/.steam/steam/steamapps"
+    sed -i 's/\("AutoUpdateBehavior".*\)"0"/\1"1"/' "${STEAM_FOLDER}"/*.acf
 }
