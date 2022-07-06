6011bbdb3531a6a92e4263788d54de6d90b0dcc8
Gilberto Vidal <gilbertovidala@gmail.com>
2022-07-06 00:36:59 -0500

    Update db

--- getaur.bash
+++ getaur.bash
@@ -6,16 +6,22 @@
 
 # Install packages
 AURREPOS=(
-	"aur/google-earth-pro"
-	"aur/lemonbar-git"
-	"aur/libxft-bgra"
-	"aur/minecraft-launcher"
-	"aur/nomachine"
-	"aur/pod2man"
-	"aur/tuxguitar"
-	"aur/vscodium-bin"
-	"aur/xtitle"
+    "aur/flashplayer-standalone"
+    "aur/google-earth-pro"
+    "aur/lemonbar-git"
+    "aur/libxft-bgra"
+    "aur/mcaselector"
+    "aur/minecraft-launcher"
+    "aur/nomachine"
+    "aur/pcloud-drive"
+    "aur/pod2man"
+    "aur/spotify"
+    "aur/tuxguitar"
+    "aur/vscodium-bin"
+    "aur/xtitle"
+    "aur/xzoom"
 )
-echo ${AURREPOS[*]}
+# Update db so yay appears on the list too!
+yay -Y --gendb
 # Unquoted so words are split into different strings
 yay -Syu --needed ${AURREPOS[*]}
