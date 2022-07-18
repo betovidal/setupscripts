25ac82ad2d1f01b5811d4fcb91d32af6b9768db4
Gilberto Vidal <gilbertovidala@gmail.com>
2022-07-18 02:53:12 -0500

    Added script to regenerate EFI entries when Windows overwrites them.

---
+++ dotfiles/.local/bin/grub-fix.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+BOOT_PATH=/boot
+if mountpoint -q $BOOT_PATH; then
+    sudo grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+else
+    echo "Path $BOOT_PATH is not a mount point."
+    echo "Make sure to mount the efi partition in /boot."
+fi
