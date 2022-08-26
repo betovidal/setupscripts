07fe0430a054e8710169ee7693a7669cdc7ecc28
Gilberto Vidal <gilbertovidala@gmail.com>
2022-08-26 01:47:41 -0500

    Script to add my own file associations.

---
+++ dotfiles/.local/bin/update-file-associations.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Query current file associations, e.g.: $xdg-mime query default video/mp4
+
+# Documents
+xdg-mime default org.pwmt.zathura.desktop application/pdf
+xdg-mime default cr3.desktop application/epub+zip
