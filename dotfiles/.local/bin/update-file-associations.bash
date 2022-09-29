6f7c3f70e82dd714b39290d75c36ecf0ec670ab9
Gilberto Vidal <gilbertovidala@gmail.com>
2022-09-29 01:40:11 -0500

    Added file associations for images.

--- dotfiles/.local/bin/update-file-associations.bash
+++ dotfiles/.local/bin/update-file-associations.bash
@@ -5,3 +5,9 @@
 # Documents
 xdg-mime default org.pwmt.zathura.desktop application/pdf
 xdg-mime default cr3.desktop application/epub+zip
+# Images + Linked .desktop file to open it with the -a flag (for gifs).
+xdg-mime default sxiv.desktop image/gif
+xdg-mime default sxiv.desktop image/png
+xdg-mime default sxiv.desktop image/jpg
+xdg-mime default sxiv.desktop image/jpeg
+xdg-mime default sxiv.desktop image/bmp
