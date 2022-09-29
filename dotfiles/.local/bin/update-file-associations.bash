2c21311deec745f55579e486d6994a0a7b4bd5ea
Gilberto Vidal <gilbertovidala@gmail.com>
2022-09-29 02:58:21 -0500

    Fixed some file associations.

--- dotfiles/.local/bin/update-file-associations.bash
+++ dotfiles/.local/bin/update-file-associations.bash
@@ -5,6 +5,18 @@
 # Documents
 xdg-mime default org.pwmt.zathura.desktop application/pdf
 xdg-mime default cr3.desktop application/epub+zip
+#.doc      application/msword
+#.dot      application/msword
+#.docx     application/vnd.openxmlformats-officedocument.wordprocessingml.document
+#.dotx     application/vnd.openxmlformats-officedocument.wordprocessingml.template
+#.docm     application/vnd.ms-word.document.macroEnabled.12
+#.dotm     application/vnd.ms-word.template.macroEnabled.12
+xdg-mime default libreoffice-writer.desktop application/msword
+xdg-mime default libreoffice-writer.desktop application/msword
+xdg-mime default libreoffice-writer.desktop application/vnd.openxmlformats-officedocument.wordprocessingml.document
+xdg-mime default libreoffice-writer.desktop application/vnd.openxmlformats-officedocument.wordprocessingml.template
+xdg-mime default libreoffice-writer.desktop application/vnd.ms-word.document.macroEnabled.12
+xdg-mime default libreoffice-writer.desktop application/vnd.ms-word.template.macroEnabled.12
 # Images + Linked .desktop file to open it with the -a flag (for gifs).
 xdg-mime default sxiv.desktop image/gif
 xdg-mime default sxiv.desktop image/png
