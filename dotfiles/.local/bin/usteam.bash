85ab9fa2956d329edb61c04c8edccd88856a1f93
Gilberto Vidal <gilbertovidala@gmail.com>
2023-08-08 12:43:11 -0600

    Better message

--- dotfiles/.local/bin/usteam.bash
+++ dotfiles/.local/bin/usteam.bash
@@ -3,5 +3,5 @@
 STEAM_FOLDER="$HOME/.steam/steam/steamapps"
 if [ -d "$STEAM_FOLDER" ]; then
     sed -i 's/\("AutoUpdateBehavior".*\)"0"/\1"1"/' "${STEAM_FOLDER}"/*.acf
-    echo "Updated steams to update on launch only."
+    echo "Modified games configurations to be updated on launch only."
 fi
