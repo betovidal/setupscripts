1bb48d3df3dcdc71d63c6fa04732f7f528db5813
Gilberto Vidal <gilbertovidala@gmail.com>
2024-01-18 01:30:28 -0600

    Updated names

--- dotfiles/.local/share/bash-completion/completions/do-backups.bash
+++ dotfiles/.local/share/bash-completion/completions/do-backups.bash
@@ -4,31 +4,20 @@
 
 # can $action $type $args
 
-SCRIPT="do-backups"
+SCRIPT="mc-backups"
 minecraft_saves_path="$HOME/.minecraft/saves/"
 minecraft_saves=$(ls "$minecraft_saves_path" | tr '\n' ' ')
 actions="backup restore"
-backup_types="minecraft wordpress bookstack"
 
-_do_backups_completions() {
+_mc_backups_completions() {
     unset COMPREPLY
     # Only complete the second argument
     # Usernames
     if [ "${#COMP_WORDS[@]}" = "2" ]; then
         COMPREPLY=($(compgen -W "$actions" "${COMP_WORDS[-1]}"))
     elif [ "${#COMP_WORDS[@]}" = "3" ]; then
-        COMPREPLY=($(compgen -W "$backup_types" "${COMP_WORDS[-1]}"))
-    elif [ "${#COMP_WORDS[@]}" = "4" ]; then
-        case "${COMP_WORDS[-2]}" in
-            minecraft)
-                COMPREPLY=($(compgen -W "$minecraft_saves" "${COMP_WORDS[-1]}"))
-                ;;
-            *)
-                words="hola amigo :)"
-                COMPREPLY=($(compgen -W "$words" "${COMP_WORDS[-1]}"))
-                ;;
-        esac
+        COMPREPLY=($(compgen -W "$minecraft_saves" "${COMP_WORDS[-1]}"))
     fi
 }
 
-complete -F _do_backups_completions "$SCRIPT"
+complete -F _mc_backups_completions "$SCRIPT"
