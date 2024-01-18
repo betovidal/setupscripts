901058407ee4b2a1e03e67734ea82ff2bedffb7b
Gilberto Vidal <gilbertovidala@gmail.com>
2024-01-17 22:49:30 -0600

    Constant at the top of the script

--- dotfiles/.local/share/bash-completion/completions/do-backups.bash
+++ dotfiles/.local/share/bash-completion/completions/do-backups.bash
@@ -5,21 +5,22 @@
 # can $action $type $args
 
 SCRIPT="do-backups"
+minecraft_saves_path="$HOME/.minecraft/saves/"
+minecraft_saves=$(ls "$minecraft_saves_path" | tr '\n' ' ')
+actions="backup restore"
+backup_types="minecraft wordpress bookstack"
+
 _do_backups_completions() {
     unset COMPREPLY
     # Only complete the second argument
     # Usernames
     if [ "${#COMP_WORDS[@]}" = "2" ]; then
-        actions="backup restore"
         COMPREPLY=($(compgen -W "$actions" "${COMP_WORDS[-1]}"))
     elif [ "${#COMP_WORDS[@]}" = "3" ]; then
-        backup_types="minecraft wordpress bookstack"
         COMPREPLY=($(compgen -W "$backup_types" "${COMP_WORDS[-1]}"))
     elif [ "${#COMP_WORDS[@]}" = "4" ]; then
         case "${COMP_WORDS[-2]}" in
             minecraft)
-                minecraft_saves_path="$HOME/.minecraft/saves/"
-                minecraft_saves=$(ls "$minecraft_saves_path" | tr '\n' ' ')
                 COMPREPLY=($(compgen -W "$minecraft_saves" "${COMP_WORDS[-1]}"))
                 ;;
             *)
