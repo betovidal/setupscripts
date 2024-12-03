84ac889cd401b50c8e5ca6c32d6beef973caa746
Gilberto Vidal <gilbertovidala@gmail.com>
2024-12-03 16:54:00 -0600

    Updated grub fix script

--- dotfiles/.local/bin/grub-fix.bash
+++ dotfiles/.local/bin/grub-fix.bash
@@ -3,6 +3,7 @@
 BOOT_PATH=/boot
 if mountpoint -q $BOOT_PATH; then
     sudo grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+    sudo grub-mkconfig -o /boot/grub/grub.cfg
 else
     echo "Path $BOOT_PATH is not a mount point."
     echo "Make sure to mount the efi partition in /boot."
